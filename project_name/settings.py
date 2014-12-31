17da0bd67abb923e7aedf150c2424d62f1cf758b
Chester Balingit <acbalingit@gmail.com>
2014-12-31 23:45:03 +0800

    generate new secret key when used as  template

--- project_name/settings.py
+++ project_name/settings.py
@@ -19,7 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'i+acxn5(akgsn!sr4^qgf(^m&*@+g1@u^t@=8s@axc41ml*f=s'
+SECRET_KEY = {{ secret_key }}
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
