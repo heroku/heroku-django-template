27cebfd748c9bfbd1ce246856f81c6f861c46923
andreas <andreas@agoodid.se>
2015-05-16 17:18:13 +0200

    remove whitespace

--- project_name/settings.py
+++ project_name/settings.py
@@ -92,7 +92,7 @@ USE_TZ = True
 
 
 # Parse database configuration from $DATABASE_URL
-DATABASES['default'] =  dj_database_url.config()
+DATABASES['default'] = dj_database_url.config()
 
 # Enable Connection Pooling (if desired)
 DATABASES['default']['ENGINE'] = 'django_postgrespool'
