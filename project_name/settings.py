e1d453c28983e64f99b1f73f75db95b995840d19
Thanasis Daglis <ath.daglis@gmail.com>
2015-12-12 16:19:20 +0200

    Updated TEMPLATES debug to use setting DEBUG
    Instead of setting TEMPLATES option debug to True, use DEBUG setting to make toggling debug mode easier.

--- project_name/settings.py
+++ project_name/settings.py
@@ -61,7 +61,7 @@ TEMPLATES = [
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
-            'debug': True,
+            'debug': DEBUG,
         },
     },
 ]
