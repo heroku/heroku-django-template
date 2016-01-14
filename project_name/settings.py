0d584a92f47de9c49a9cddf2bd066524a08f094f
Kenneth Reitz <me@kennethreitz.org>
2016-01-13 19:03:15 -0500

    remote django-postgres-pool

--- project_name/settings.py
+++ project_name/settings.py
@@ -94,8 +94,8 @@ USE_TZ = True
 # Parse database configuration from $DATABASE_URL
 DATABASES['default'] = dj_database_url.config()
 
-# Enable Connection Pooling (if desired)
-DATABASES['default']['ENGINE'] = 'django_postgrespool'
+# Enable Persistent Connections
+DATABASES['default']['CONN_MAX_AGE'] = 500
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
