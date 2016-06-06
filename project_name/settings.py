990573f5d225533dc5451f2fe597993d2ce95e2b
Ed Morley <emorley@mozilla.com>
2016-06-06 14:19:51 +0100

    Sync with changes made to default Django 1.9 project templates
    Brings the templates in sync with:
    https://github.com/django/django/blob/1.9.7/django/conf/project_template/project_name/settings.py-tpl
    https://github.com/django/django/blob/1.9.7/django/conf/project_template/project_name/urls.py-tpl
    https://github.com/django/django/blob/1.9.7/django/conf/project_template/project_name/wsgi.py-tpl

--- project_name/settings.py
+++ project_name/settings.py
@@ -28,16 +28,17 @@ DEBUG = True
 
 # Application definition
 
-INSTALLED_APPS = (
+INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-)
+]
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -45,12 +46,11 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.security.SecurityMiddleware',
-)
+]
 
 ROOT_URLCONF = '{{ project_name }}.urls'
 
-TEMPLATES = (
+TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [],
@@ -65,7 +65,7 @@ TEMPLATES = (
             'debug': DEBUG,
         },
     },
-)
+]
 
 WSGI_APPLICATION = '{{ project_name}}.wsgi.application'
 
@@ -80,7 +80,7 @@ DATABASES = {
     }
 }
 
-AUTH_PASSWORD_VALIDATORS = (
+AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
     },
@@ -93,7 +93,7 @@ AUTH_PASSWORD_VALIDATORS = (
     {
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
-)
+]
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.9/topics/i18n/
--- project_name/wsgi.py
+++ project_name/wsgi.py
@@ -4,11 +4,12 @@ WSGI config for {{ project_name }} project.
 It exposes the WSGI callable as a module-level variable named ``application``.
 
 For more information on this file, see
-https://docs.djangoproject.com/en/1.7/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
-
 import os
+
+# Must be defined prior to importing `whitenoise.django`.
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "{{ project_name }}.settings")
 
 from django.core.wsgi import get_wsgi_application
