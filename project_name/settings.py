a396f531045dcbdffa7bee529338670343ec1ca9
Travis Jungroth <jungroth@gmail.com>
2016-10-25 20:01:44 -0700

    Fix typo

--- project_name/settings.py
+++ project_name/settings.py
@@ -1,5 +1,5 @@
 """
-Django settings for {{ project_name }} project on Heroku. Fore more info, see:
+Django settings for {{ project_name }} project on Heroku. For more info, see:
 https://github.com/heroku/heroku-django-template
 
 For more information on this file, see
