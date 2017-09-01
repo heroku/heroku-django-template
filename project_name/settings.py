d1a9436976d89147dfd718f71487aa029244efe9
Rishabh Agrahari <rishabhiitbhu@users.noreply.github.com>
2017-09-02 01:33:07 +0530

    Removed assignment.

--- project_name/settings.py
+++ project_name/settings.py
@@ -109,8 +109,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Change 'default' database configuration with $DATABASE_URL.
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500))
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
