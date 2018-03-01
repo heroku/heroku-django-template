019ac69e97a063c9e71462f64b0f6ba7c34eed47
Kenneth Reitz <me@kennethreitz.org>
2018-03-01 15:05:31 -0500

    updated settings
    Signed-off-by: Kenneth Reitz <me@kennethreitz.org>

--- project_name/settings.py
+++ project_name/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -131,3 +132,6 @@ STATICFILES_DIRS = [
 # Simplified static file serving.
 # https://warehouse.python.org/project/whitenoise/
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
