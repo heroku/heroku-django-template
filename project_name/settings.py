d131695d04ff6ab36a23d46b2b0f9137dc02285f
Kenneth Reitz <me@kennethreitz.org>
2018-03-02 08:39:23 -0500

    Update settings.py

--- project_name/settings.py
+++ project_name/settings.py
@@ -110,7 +110,7 @@ USE_L10N = True
 USE_TZ = True
 
 # Change 'default' database configuration with $DATABASE_URL.
-DATABASES['default'].update(dj_database_url.config(conn_max_age=500, require_ssl=True))
+DATABASES['default'].update(dj_database_url.config(conn_max_age=500, ssl_require=True))
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
